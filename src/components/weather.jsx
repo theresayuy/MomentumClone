aba37368929defcaeb143906b4a7b06a9d615bee
Theresa Yu <69079112+theresayuy@users.noreply.github.com>
2022-08-30 03:18:39 -0400

    Updated weather.jsx
    - changed class component to functional component
    - uses jquery to set the weather upon loading

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,43 +1,34 @@
-import React from 'react';
 import '../App.css';
 import $ from 'jquery';
 import { OPENWM_KEY } from './constants/apikey';
 import { getDeepCopy } from './helpers/str-arr-stuff';
 
-class Weather extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            temperature: "0°C",
-            condition: ""
-        }
-    }
+function setWeather() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
+        let baseURL = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${OPENWM_KEY}`;
+    
+        $.get(baseURL, function(res) {
+            // I have no clue how this jquery stuff works!!!!!!!!!
+            let data = res.current;
+            let temp = Math.floor(data.temp - 273);
+            let condition = data.weather[0].description;
+            $('#temperature').html(`${temp}°C`);
+            $('#condition').html(condition);
+        })
+    });
+}
 
-    render() {
-        getGeolocation();
-        return (
-            <span className="Weather-Main">
-                <div id="temperature">{this.state.temperature}</div>
-                <div id="condition">{this.state.condition}</div>
-            </span>
-        );
-    }
+function Weather() {
+    setWeather();
 
-    getGeolocation() {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            let lat = position.coords.latitude;
-            let long = position.coords.longitude;
-            let baseURL = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${OPENWM_KEY}`;
-        
-            $.get(baseURL, function(result) {
-                let data = result.current;
-                let temp = Math.floor(data.temp - 273); // convert to celcius
-                let cond = data.weather[0].description;
-                this.state.temperature = `${temp}°C`;
-                this.state.condition = getDeepCopy(cond);
-            });
-        }); 
-    }
+    return(
+        <div className="Weather-Main">
+            <div id="temperature"></div>
+            <div id="condition"></div>
+        </div> 
+    );
 }
 
 export default Weather;
