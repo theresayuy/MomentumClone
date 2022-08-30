37371759d798a3d8dd202534bab6409ebc208ba2
Theresa Yu <69079112+theresayuy@users.noreply.github.com>
2022-08-30 03:39:25 -0400

    Background Image consistently loads now
    - added constants for background and overlay CSS in bg.jsx
    - removed CSS for background and overlay from App.css

--- src/components/bg.jsx
+++ src/components/bg.jsx
@@ -3,15 +3,34 @@ import $ from 'jquery';
 import '../App.css';
 
 function Background() {
-    // set the background image from lorem picsum 
-    setTimeout(() => {
-        $("#bg").css("background-image", `url(https://picsum.photos/${$(window).width()}/${$(window).height()})`);
-        $("#overlay").css("background-color", "rgba(0, 0, 0, 0.4)");
-    }, 20);
+    const BG_CSS = {
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        position: "absolute",
+        top: "0",
+        left: "0",
+        height: "100%",
+        width: "100%",
+        zIndex: "-1",
+        backgroundImage: `url(https://picsum.photos/${$(window).width()}/${$(window).height()})`
+    };
+    const OVERLAY_CSS = {
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        position: "absolute",
+        top: "0",
+        left: "0",
+        height: "100%",
+        width: "100%",
+        zIndex: "-1",
+        backgroundColor: "rgba(0, 0, 0, 0.4)"
+    }
     
     return (
-        <div id="bg">
-            <div id="overlay"></div>
+        <div id="bg" style={BG_CSS}>
+            <div id="overlay" style={OVERLAY_CSS}></div>
         </div>
     );
 }
